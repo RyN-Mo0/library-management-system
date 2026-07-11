2cad57fd2b470a4a03212ec4ffe55db478f3cdd9
RyN <57rpj5zpjs@privaterelay.appleid.com>
2026-07-11 07:10:04 +0300

    Refactor Member class to include password field
    Added password field and updated constructor in Member class.

--- Member.java
+++ Member.java
@@ -1,18 +1,19 @@
-
 package com.mycompany.library;
 
 import java.util.ArrayList;
 
 public class Member {
-    private String name;
-    private String memberId;
-    private ArrayList<Book> borrowedBooks;
+    protected String name;
+    protected String memberId;
+    protected String password;
+    protected final ArrayList<Book> borrowedBooks;
 
 
-    public Member(String name, String memberId) {
+    public Member(String name, String memberId, String password) {
         this.name = name;
         this.memberId = memberId;
-        this.borrowedBooks = new ArrayList<Book>();
+        this.password = password;
+        this.borrowedBooks = new ArrayList<>();
     }
 
     public String getName() {
@@ -22,14 +23,12 @@ public class Member {
     public String getMemberId() {
         return memberId;
     }
+    
+    public String getPassword() {
+        return password;
+    }
 
     public ArrayList<Book> getBorrowedBooks() {
         return borrowedBooks;
     }
-
-
-
-
-
-
 }
